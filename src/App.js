93ca5ff98b6c05da0f7b3c0facf408664b69efe1
ShoutingEagle <sameerpaul58@gmail.com>
2023-12-17 06:42:15 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [confirmPassword,setConfirmPassword] = useState('');
+  const [data,setData] = useState('')
+  const [matchedEmail,setMatchedEmail] = useState('');
+  const [passwordLength,setPasswordLength] = useState('');
+  const [matchedPassword,setMatchedPassword] = useState('');
+  const [errorEmail,setErrorEmail] = useState('');
+  const [errorPassword,setErrorPassword] = useState('');
+  const [errorConfirmPassword,setErrorConfirmPassword] = useState('');
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setData({
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword
+    });
+
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+
+    if(!data.email.includes('@') && !data.email.includes('.') ){
+      setErrorEmail('Invalid Email Format');
+      setMatchedEmail(false);
+      return;
+    }
+    else{
+      setMatchedEmail(true);
+    }
+
+
+    if(data.password.length<8){
+      setErrorPassword("Password must be atlest 8 characters");
+      setPasswordLength(false);
+      return;
+    }
+    else{
+      setPasswordLength(true);
+    }
+
+
+    if(data.password !== data.confirmPassword){
+      setErrorConfirmPassword("Passwords do not match");
+      setMatchedPassword(false);
+      return;
+    }
+    else{
+      setMatchedPassword(true);
+    }
+    
+
+    
+
+    alert('Login Successfull');
+
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='form' onSubmit={handleSubmit}>
+
+
+
+        <div className='element'>
+          <label htmlFor='email'>Email</label>
+          <input 
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            value={email}
+            placeholder="example@domain.com"
+            className='email'
+            required/>
+          <p>{errorEmail}</p>
+        </div>
+
+
+
+        <div className='element'> 
+          <label htmlFor='password'>Password</label>
+          <input 
+          type='password' 
+          name='password' 
+          placeholder='Enter Password' 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)}
+          className='password'
+          required />
+          <p>{errorPassword}</p>
+        </div>
+
+
+
+        <div className='element'>
+          <label htmlFor='confirmPassword'>Confirm Password</label>
+          <input 
+          type='password' 
+          name='confirmPassword' 
+          placeholder='Confirm Password' 
+          value={confirmPassword}  
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required/>
+          <p>{errorConfirmPassword}</p>
+        </div>
+        <div>
+          <button type='submit'>Signup</button>
+        </div>
+      </form>
     </div>
   );
 }
